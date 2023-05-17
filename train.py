938bfa6d61bc846a76fab821dd202b17947d3e6b
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-05-17 10:00:17 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
-from torch.utils.tensorboard import SummaryWritter
+from torch.utils.tensorboard import SummaryWriter
 from utils import save_checkpoint, load_checkpoint, print_examples
 
-from loader import get_loader
+from get_loader import get_loader
 from model import CNNtoRNN
 
 def train():
@@ -37,5 +37,45 @@ def train():
     learning_rate = 3e-4
     num_epochs = 100
     num_layers = 1
- 
+    
+    #for tensorboard
+    writer = SummaryWriter("runs/flickr") 
+    step=0
+    
+    #Initialize model, loss...
+    model = CNNtoRNN(embed_size, hidden_size, vocab_size, num_layers).to(device)
+    criterion = nn.CrossEntropyLoss(ignore_index=dataset.vocab.stoi["<PAD>"])
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    
+    if load_model:
+        step = load_checkpoint(torch.load("my_checkpoint.pth.tar"), model, optimizer)
+        
+    model.train()
+    
+    for epoch in range(num_epochs):
+        print_examples(model, device, dataset)
+        if save_model:
+            checkpoint = {
+				"state_dict" : model.state_dict(),
+				"optimizer" : optimizer.state_dict(),
+				"step" : step,
+				}
+            save_checkpoint(checkpoint)
+            
+        for idx, (imgs, captions) in enumerate(train_loader):
+            imgs = imgs.to(device)
+            captions = captions.to(device)
+            
+            outputs = model(imgs, captions[:-1]) #we want the model to predict the end token
+            loss = criterion(outputs.reshape(-1, outputs.shape[2]), captions.reshape(-1))
+            
+            writer.add_scalar("Training loss", loss.item(), global_step = step)
+            step += 1
+            
+            optimizer.zero_grad()
+            loss.backward(loss)
+            optimizer.step()
+
+if __name__ == "__main__":
+    train()
  
