3d5dc9bcbd99bd34c855517d8f3591988d797464
Ubuntu <xnmaster@ML-RefVm-536361.k1cflzs2p0se5gllqwccownfma.ax.internal.cloudapp.net>
2023-05-21 13:01:24 +0000

    correctly evaluating with bleu score

--- evaluation.py
+++ evaluation.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 from nltk.translate.bleu_score import sentence_bleu
 import matplotlib.pyplot as plt
+
 from utils import save_checkpoint, load_checkpoint, print_and_export_examples
 from get_loader import get_loader
 from model import CNNtoRNN
@@ -15,6 +16,7 @@ def evaluate_model(model, dataset, device):
 
     with torch.no_grad():
         for idx in range(len(dataset)):
+            print(f"Evaluating example {idx+1}/{len(dataset)}")
             image, captions = dataset[idx]
             image = image.unsqueeze(0).to(device)
 
@@ -22,16 +24,34 @@ def evaluate_model(model, dataset, device):
             generated_caption = model.caption_image(image, dataset.vocab)
             generated_caption = ' '.join(generated_caption)
 
+            print(f"Generated Caption: {generated_caption}")
+
+            # Convert the captions from tensors to lists of strings
+            if isinstance(captions, list):
+                reference_captions = [
+                    list(map(dataset.vocab.itos.__getitem__, caption.tolist()))
+                    for caption in captions
+                ]
+            else:
+                reference_captions = [
+                    list(map(dataset.vocab.itos.__getitem__, captions.tolist()))
+                ]
+
+            print("Reference Captions:")
+            for reference_caption in reference_captions:
+                print(' '.join(reference_caption))
+
             # Compute the BLEU score for each reference caption
-            reference_captions = [' '.join(caption) for caption in captions]
             bleu_scores_per_caption = [
-                sentence_bleu([reference_caption], generated_caption)
+                sentence_bleu(reference_caption, generated_caption)
                 for reference_caption in reference_captions
             ]
 
             # Take the maximum BLEU score among all the reference captions
             max_bleu_score = max(bleu_scores_per_caption)
             bleu_scores.append(max_bleu_score)
+
+            print(f"BLEU Score for Example {idx+1}: {max_bleu_score:.4f}\n")
 
     average_bleu = sum(bleu_scores) / len(bleu_scores)
     print(f"Average BLEU Score: {average_bleu:.4f}")
@@ -42,9 +62,13 @@ def visualize_predictions(model, dataset, device, num_examples=5):
 
     with torch.no_grad():
         for idx in range(num_examples):
+            print(f"Visualizing example {idx+1}/{num_examples}")
             image, captions = dataset[idx]
             original_image = transforms.ToPILImage()(image).convert("RGB")
             image = image.unsqueeze(0).to(device)
+
+            # Convert the captions from indices to words
+            captions = [[dataset.vocab.itos[idx] for idx in caption] for caption in captions]
 
             # Generate caption using the model
             generated_caption = model.caption_image(image, dataset.vocab)
@@ -75,10 +99,9 @@ def main():
     ])
 
     # Specify the paths to the trained model checkpoint and evaluation dataset
-    model_checkpoint_path = "checkpoint.pth"
-    images_path = "/Users/nde-la-f/Documents/Image_caption/flickr8k/Images/"
-    annotations_path = "/Users/nde-la-f/Documents/Image_caption/flickr8k/captions.txt"
-
+    model_checkpoint_path = "/home/xnmaster/PIXTALES-2/checkpoint.pth"
+    images_path = "/home/xnmaster/PIXTALES-2/Images/"
+    annotations_path = "/home/xnmaster/PIXTALES-2/captions.txt"
 
     # Set CUDA benchmark for improved performance
     torch.backends.cudnn.benchmark = True
@@ -99,10 +122,10 @@ def main():
     checkpoint = torch.load(model_checkpoint_path)
 
     # Get the model parameters from the checkpoint
-    embed_size = checkpoint["embed_size"]
-    hidden_size = checkpoint["hidden_size"]
-    vocab_size = checkpoint["vocab_size"]
-    num_layers = checkpoint["num_layers"]
+    embed_size = 256
+    hidden_size = 256
+    vocab_size = len(eval_dataset.vocab)
+    num_layers = 1
 
     # Create the model and load the checkpoint parameters
     model = CNNtoRNN(embed_size, hidden_size, vocab_size, num_layers)
@@ -110,9 +133,11 @@ def main():
     model = model.to(device)
 
     # Evaluate the model
+    print("Starting model evaluation...")
     evaluate_model(model, eval_dataset, device)
 
     # Visualize predictions
+    print("\nStarting visualization of predictions...")
     visualize_predictions(model, eval_dataset, device, num_examples=5)
 
 
