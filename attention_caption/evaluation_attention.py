59670bb7dacb869685ff7716ca7cc429e3cdf0e2
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-05-27 13:43:49 +0200

    Not yet fully implementedevaluation_attention.py

--- attention_caption/evaluation_attention.py
+++ attention_caption/evaluation_attention.py
@@ -2,7 +2,13 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
+from nltk.translate.bleu_score import corpus_bleu
 from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.meteor_score import meteor_score
+from nltk.translate.nist_score import sentence_nist
+from nltk.translate.ribes_score import sentence_ribes
+from pycocotools.coco import COCO
+from pycocoevalcap.eval import COCOEvalCap
 import matplotlib.pyplot as plt
 import os
 import base64
@@ -13,9 +19,14 @@ from utils_attention import save_checkpoint, load_checkpoint
 from get_loader_attention import get_loader
 from model_attention import CNNtoRNN
 
+
 def evaluate_model(model, dataset, device):
     model.eval()  # Set the model to evaluation mode
     bleu_scores = []  # Store the BLEU scores for each example
+    cider_scores = []  # Store the CIDEr scores for each example
+    meteor_scores = []  # Store the METEOR scores for each example
+    nist_scores = []  # Store the NIST scores for each example
+    ribes_scores = []  # Store the RIBES scores for each example
     best_bleu_score = 0  # Keep track of the highest BLEU score
 
     # Create and open the HTML file for writing
@@ -80,17 +91,87 @@ def evaluate_model(model, dataset, device):
                     html_file.write("<p>" + ' '.join(reference_caption) + "</p>")
                 html_file.write("<p>BLEU Score: " + str(max_bleu_score) + "</p>")
 
+            # Compute the CIDEr score
+            cider_scores.append(compute_cider_score(reference_captions, generated_caption))
+
+            # Compute the METEOR score
+            meteor_scores.append(compute_meteor_score(reference_captions, generated_caption))
+
+            # Compute the NIST score
+            nist_scores.append(compute_nist_score(reference_captions, generated_caption))
+
+            # Compute the RIBES score
+            ribes_scores.append(compute_ribes_score(reference_captions, generated_caption))
+
     average_bleu = sum(bleu_scores) / len(bleu_scores)
+    average_cider = sum(cider_scores) / len(cider_scores)
+    average_meteor = sum(meteor_scores) / len(meteor_scores)
+    average_nist = sum(nist_scores) / len(nist_scores)
+    average_ribes = sum(ribes_scores) / len(ribes_scores)
+
     print(f"Average BLEU Score: {average_bleu:.4f}")
+    print(f"Average CIDEr Score: {average_cider:.4f}")
+    print(f"Average METEOR Score: {average_meteor:.4f}")
+    print(f"Average NIST Score: {average_nist:.4f}")
+    print(f"Average RIBES Score: {average_ribes:.4f}")
 
     # Close the HTML file
     html_file.write("</body></html>")
     html_file.close()
 
+
+def compute_cider_score(reference_captions, generated_caption):
+    # Create COCO object and set up the reference and hypothesis lists
+    coco = COCO()
+    coco.dataset['refs'] = [{'image_id': 0, 'caption': ref_caption} for ref_caption in reference_captions]
+    coco.dataset['hyps'] = [{'image_id': 0, 'caption': generated_caption}]
+    
+    # Create COCOEvalCap object for scoring
+    coco_eval = COCOEvalCap(coco)
+
+    # Compute CIDEr score
+    coco_eval.evaluate()
+
+    # Return CIDEr score
+    cider_score = coco_eval.eval['CIDEr']
+    return cider_score
+
+
+def compute_meteor_score(reference_captions, generated_caption):
+    # Convert reference and hypothesis captions to strings
+    reference_captions = [' '.join(ref_caption) for ref_caption in reference_captions]
+    generated_caption = ' '.join(generated_caption)
+
+    # Compute METEOR score
+    meteor_score_value = meteor_score(reference_captions, generated_caption)
+    return meteor_score_value
+
+
+def compute_nist_score(reference_captions, generated_caption):
+    # Convert reference and hypothesis captions to lists of lists of tokens
+    reference_captions = [ref_caption.split() for ref_caption in reference_captions]
+    generated_caption = generated_caption.split()
+
+    # Compute NIST score
+    nist_score = sentence_nist(reference_captions, generated_caption)
+    return nist_score
+
+
+def compute_ribes_score(reference_captions, generated_caption):
+    # Convert reference and hypothesis captions to lists of lists of tokens
+    reference_captions = [ref_caption.split() for ref_caption in reference_captions]
+    generated_caption = generated_caption.split()
+
+    # Compute RIBES score
+    ribes_score = sentence_ribes(reference_captions, generated_caption)
+    return ribes_score
+
+
 def denormalize(image):
     mean = torch.tensor([0.5, 0.5, 0.5]).reshape(1, 3, 1, 1)
     std = torch.tensor([0.5, 0.5, 0.5]).reshape(1, 3, 1, 1)
     return image * std + mean
+
 
 def visualize_predictions(model, dataset, device, num_examples=5):
     model.eval()  # Set the model to evaluation mode
@@ -110,7 +191,7 @@ def visualize_predictions(model, dataset, device, num_examples=5):
                 captions = [dataset.vocab.itos[captions.item()]]
 
             # Generate caption using the model
-            generated_caption = model.caption_image(image, dataset.vocab)
+            generated_caption, attention_maps = model.caption_image(image, dataset.vocab, return_attention=True)
             generated_caption = ' '.join(generated_caption)
 
             # Print original captions
@@ -127,21 +208,30 @@ def visualize_predictions(model, dataset, device, num_examples=5):
             plt.axis("off")
             plt.show()
 
+            # Display attention maps
+            plt.figure(figsize=(10, 10))
+            num_words = len(generated_caption.split())
+            for i in range(num_words):
+                plt.subplot(num_words // 5 + 1, 5, i + 1)
+                plt.imshow(attention_maps[i].squeeze().cpu(), cmap='gray')
+                plt.axis("off")
+                plt.title(generated_caption.split()[i])
+            plt.tight_layout()
+            plt.show()
 
 
 def main():
     # Define the image transformations for evaluation
     transform_eval = transforms.Compose([
-        transforms.Resize((356, 356)),
-        transforms.CenterCrop((299, 299)),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
     # Specify the paths to the trained model checkpoint and evaluation dataset
-    model_checkpoint_path = "/home/xnmaster/PIXTALES-2/checkpoint8.pth" #change it maiol, dani
-    images_path = "/home/xnmaster/PIXTALES-2/Images/" #change it maiol, dani
-    annotations_path = "/home/xnmaster/PIXTALES-2/captions.txt" #change it maiol, dani
+    model_checkpoint_path = "path/to/your/checkpoint.pth"
+    images_path = "path/to/your/images/"
+    annotations_path = "path/to/your/annotations.txt"
 
     # Set CUDA benchmark for improved performance
     torch.backends.cudnn.benchmark = True
